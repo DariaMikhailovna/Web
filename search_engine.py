6327d3dc84a0cc0630d2a7c43a43c6f5b35bae70
MargoAddams <0610-1994@mail.ru>
2019-12-06 06:00:40 +0300

    Добавила программу для поиска по тегу в гугле

---
+++ search_engine.py
@@ -0,0 +1,73 @@
+import requests
+from bs4 import BeautifulSoup as BS
+
+
+def get_link(site, tag):
+    return f'https://www.{site}.ru/search?q={tag.capitalize()}'
+
+
+def get_page(link):
+    return requests.get(link).text
+
+
+def get_page_links(link):
+    if link == 'https://pikabu.ru/tag/FGH':
+        return []
+    html_content = get_page(link)
+    soup = BS(html_content, features='html.parser')
+    links = set(link['href'] for link in soup.find_all('a', href=True))
+    return links
+
+
+def get_google_page_links(link):
+    html_content = get_page(link)
+    soup = BS(html_content, features='html.parser')
+    links = []
+    for element in soup.find_all('a', target="_blank", rel="noopener"):
+        link = element['href']
+        prefix = '/url?q='
+        if link.startswith(prefix):
+            link = link[len(prefix):]
+        if '&' in link:
+            index = link.index('&')
+            link = link[:index]
+        links.append(link)
+    return links
+
+
+def get_links(link_query, is_rec):
+    links = get_google_page_links(link_query)
+    links = [link for link in links if link.startswith('http')]
+    neighboring_links = []
+    if is_rec:
+        for link in links:
+            neighboring_links += get_page_links(link)
+    links += neighboring_links
+    links = set(link for link in links if link.startswith('http'))
+    return links
+
+
+def main():
+    site = 'google'
+    tag = input('Введите тег запроса:')
+    max_links_count = input('Введите максимальное выводимое количество ссылок:')
+    is_rec = input('Введите да, если хотите запустить рекурсивный поиск и нет, если не рекурсивный:')
+    if not max_links_count.isdigit():
+        print('Количество должно быть числом')
+    else:
+        max_links_count = int(max_links_count)
+    if is_rec == 'да':
+        is_rec = True
+    elif is_rec == 'нет':
+        is_rec = False
+    else:
+        print('Вы ошиблись в ответе на вопрос про рекурсию')
+        exit()
+    link_query = get_link(site, tag)
+    links = list(get_links(link_query, is_rec))
+    for link in links[:max_links_count]:
+        print(link)
+
+
+if __name__ == '__main__':
+    main()
