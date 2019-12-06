697dda4883f50b126b8040420da280bbf75f11e6
MargoAddams <0610-1994@mail.ru>
2019-12-06 06:21:10 +0300

    добавила исключение

--- search_engine.py
+++ search_engine.py
@@ -11,12 +11,13 @@ def get_page(link):
 
 
 def get_page_links(link):
-    if link == 'https://pikabu.ru/tag/FGH':
+    try:
+        html_content = get_page(link)
+        soup = BS(html_content, features='html.parser')
+        links = set(link['href'] for link in soup.find_all('a', href=True))
+        return links
+    except requests.exceptions.ConnectionError:
         return []
-    html_content = get_page(link)
-    soup = BS(html_content, features='html.parser')
-    links = set(link['href'] for link in soup.find_all('a', href=True))
-    return links
 
 
 def get_google_page_links(link):
@@ -51,15 +52,15 @@ def main():
     site = 'google'
     tag = input('Введите тег запроса:')
     max_links_count = input('Введите максимальное выводимое количество ссылок:')
-    is_rec = input('Введите да, если хотите запустить рекурсивный поиск и нет, если не рекурсивный:')
+    is_rec = input('Введите "yes", если хотите запустить рекурсивный поиск и "no", если не рекурсивный:')
     if not max_links_count.isdigit():
         print('Количество должно быть числом')
         exit()
     else:
         max_links_count = int(max_links_count)
-    if is_rec == 'да':
+    if is_rec == 'yes':
         is_rec = True
-    elif is_rec == 'нет':
+    elif is_rec == 'no':
         is_rec = False
     else:
         print('Вы ошиблись в ответе на вопрос про рекурсию')
