11bc255de9b93f4de1ec9e0a2d0bcbb8e915905f
MargoAddams <0610-1994@mail.ru>
2020-01-10 05:32:42 +0300

    Добавила фикстуры в тесты поисковика

--- my_search_engine/tests/test_search_engine.py
+++ my_search_engine/tests/test_search_engine.py
@@ -9,19 +9,33 @@ def test_get_link():
     assert get_link('Go', 'lol') == 'https://www.Go.ru/search?q=Lol'
 
 
-def test_get_page():
+@pytest.fixture()
+def get_html_text():
     with open('text.html', 'r') as f:
-        text = f.read()
-    assert get_page('https://www.wolframalpha.com').strip() == text.strip()
+        text = f.read().strip()
+    return text
 
 
-def test_get_page_links():
+def test_get_page(get_html_text):
+    assert get_page('https://www.wolframalpha.com').strip() == get_html_text
+
+
+@pytest.fixture()
+def get_test_links():
     with open('links.txt', 'r') as f:
-        text = f.read()
-    test_links = text.split()
-    test_links.sort()
+        links = f.read().split()
+        links.sort()
+    return links
+
+
+@pytest.fixture()
+def get_links():
     links = list(get_page_links('https://www.wolframalpha.com'))
     links.sort()
-    for i in range(len(links) - 1):
-        assert test_links[i] == links[i]
+    return links
+
+
+def test_get_page_links(get_test_links, get_links):
+    for i in range(len(get_links) - 1):
+        assert get_test_links[i] == get_links[i]
 
