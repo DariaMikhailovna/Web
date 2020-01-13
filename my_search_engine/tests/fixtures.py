34897969699e3805b74596b4e6ddd23582f05250
MargoAddams <0610-1994@mail.ru>
2020-01-13 15:27:27 +0300

    Добавила тестов

--- my_search_engine/tests/fixtures.py
+++ my_search_engine/tests/fixtures.py
@@ -1,24 +1,42 @@
 import pytest
-from search_engine import get_page_links
+from search_engine import *
 
 
-@pytest.fixture()
-def get_test_links():
+@pytest.fixture
+def test_links():
     with open('links.txt', 'r') as f:
         links = f.read().split()
         links.sort()
     return links
 
 
-@pytest.fixture()
-def get_links():
+@pytest.fixture
+def page_links():
     links = list(get_page_links('https://www.wolframalpha.com'))
     links.sort()
     return links
 
 
-@pytest.fixture()
-def get_html_text():
+@pytest.fixture
+def html_text():
     with open('text.html', 'r') as f:
         text = f.read().strip()
     return text
+
+
+link = 'https://www.google.ru/search?q=wolframalpha'
+
+
+@pytest.fixture
+def google_page_links():
+    return get_google_page_links(link)
+
+
+@pytest.fixture
+def links_no_rec():
+    return get_links(link, False)
+
+
+@pytest.fixture
+def links_is_rec():
+    return get_links(link, True)
--- my_search_engine/tests/test_search_engine.py
+++ my_search_engine/tests/test_search_engine.py
@@ -1,5 +1,4 @@
 import pytest
-from search_engine import *
 from .fixtures import *
 
 
@@ -8,13 +7,30 @@ def test_get_link():
     assert get_link('aaa', 'AaaaA') == 'https://www.aaa.ru/search?q=Aaaaa'
     assert get_link('qwerty', 'qwerty') == 'https://www.qwerty.ru/search?q=Qwerty'
     assert get_link('Go', 'lol') == 'https://www.Go.ru/search?q=Lol'
+    assert get_link('', '') == 'https://www..ru/search?q='
 
 
-def test_get_page(get_html_text):
-    assert get_page('https://www.wolframalpha.com').strip() == get_html_text
+def test_get_page(html_text):
+    assert get_page('https://www.wolframalpha.com').strip() == html_text
 
 
-def test_get_page_links(get_test_links, get_links):
-    for i in range(len(get_links) - 1):
-        assert get_test_links[i] == get_links[i]
+def test_get_page_links(test_links, page_links):
+    assert len(test_links) == len(page_links)
+    for i in range(len(page_links) - 1):
+        assert test_links[i] == page_links[i]
+
+
+def test_get_google_page_links(google_page_links):
+    assert type(google_page_links) == list
+    assert len(google_page_links) == 16
+
+
+def test_get_links_no_rec(links_no_rec):
+    assert type(links_no_rec) == set
+    assert 10 < len(links_no_rec) < 20  # т.к. выдача не статична
+
+
+def test_get_links_is_rec(links_is_rec):
+    assert type(links_is_rec) == set
+    assert 600 < len(links_is_rec) < 800  # т.к. сайты не статичны
 
